13bd1e20442629e95e67a241605e272f3fc655f3
Phuong20042910 <phuongpdtse182721@fpt.edu.vn>
2026-03-29 00:01:56 +0700

    Delete code

--- MapHome_backend_NodeJS/src/routes/transactionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getMyTransactions } = require("../controllers/transactionController");
-const { authMiddleware } = require("../middleware/authMiddleware");
-
-router.get("/me", authMiddleware, getMyTransactions);
-
-module.exports = router;
